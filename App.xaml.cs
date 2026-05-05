3bb54e4474d081211aa27f1a0e8c2d69b98e6d1b
OsakwePeter <st10497473@imconnect.edu.za>
2026-05-05 10:46:35 +0200

    Initial commit: Set up WPF project structure and application resources

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace CybersecurityChatbotWPF
+{
+    /// <summary>
+    /// Application entry-point code-behind.
+    /// </summary>
+    public partial class App : Application { }
+}
